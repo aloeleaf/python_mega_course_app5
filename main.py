daf475d9234504d62119d5c68de4669de36d9e3d
aloeleaf <szabolcs.sandor@gmail.com>
2023-06-16 16:54:20 +0200

    Day 28 : Add links, specify language, limit news, add subject, controll category

--- main.py
+++ main.py
@@ -3,10 +3,14 @@ import send_mail
 
 
 api_key = "4990955fc62b4ce7a32c952b8d7ec4ef"
+topic = "tesla"
 
-url= "https://newsapi.org/v2/everything?q=tesla" \
-     "&from=2023-05-16&sortBy=publishedAt" \
-     "&apiKey=4990955fc62b4ce7a32c952b8d7ec4ef"
+url= "https://newsapi.org/v2/everything?" \
+     f"q={topic}" \
+     "&from=2023-05-16" \
+     "&sortBy=publishedAt" \
+     "&apiKey=4990955fc62b4ce7a32c952b8d7ec4ef" \
+     "&language=en"
 
 # Make request
 request = requests.get(url)
@@ -17,9 +21,11 @@ content = request.json()
 message = str()
 
 # Access the articles and description
-for articles in content["articles"]:
-    if articles["title"] is not None:
-     #message += f'Article Title: {articles["title"]} \n\nArticle Description: {articles["description"]} \n\n\n\n'
-     message = message +  articles["title"] + "\n" + articles["description"] + 2*"\n"
+for article in content["articles"][:20]:
+    if article["title"] is not None:
+     message += "Subject: Today's news" + "\n" \
+                + article["title"] + "\n" \
+                + article["description"] + "\n" \
+                + article["url"] + "\n" + 2*"\n"
 
 send_mail.send_mail(message, "sandorsz@birosag.hu") 
